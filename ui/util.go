8c9a6e400db2f062825729517162649a564748ab
Drew Weymouth <drew.weymouth@openly.com>
2022-12-23 14:55:47 -0800

    update slider once per second always

--- ui/util.go
+++ ui/util.go
@@ -1,13 +1,16 @@
 package ui
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func SecondsToTimeString(s float64) string {
 	if s < 0 {
 		s = 0
 	}
 	min := int(s / 60)
-	sec := int(s - float64(min*60))
+	sec := int(math.Round(s - float64(min*60)))
 
 	return fmt.Sprintf("%2d:%02d", min, sec)
 }
