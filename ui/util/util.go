586c694e59b9ddce8be636e1fe3e748141982d46
Drew Weymouth <dweymouth@gmail.com>
2022-12-27 12:25:54 -0800

    fix minute changeover bug in secondsToTimeString

--- ui/util/util.go
+++ ui/util/util.go
@@ -9,8 +9,9 @@ func SecondsToTimeString(s float64) string {
 	if s < 0 {
 		s = 0
 	}
-	min := int(s / 60)
-	sec := int(math.Round(s - float64(min*60)))
+	sec := int(math.Round(s))
+	min := sec / 60
+	sec -= min * 60
 
 	return fmt.Sprintf("%2d:%02d", min, sec)
 }
