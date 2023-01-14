ece26efafb742f665502ca80afd0a31655ec5a19
Drew Weymouth <dweymouth@gmail.com>
2023-01-14 12:38:27 -0800

    add playlist icon + fyne bundled.go

---
+++ res/bundled_gen.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+fyne bundle -package res icons/disc.png > bundled.go
+fyne bundle -append icons/disc-invert.png >> bundled.go
+fyne bundle -append icons/headphones.png >> bundled.go
+fyne bundle -append icons/headphones-invert.png >> bundled.go
+fyne bundle -append icons/heart-filled.png >> bundled.go
+fyne bundle -append icons/heart-filled-invert.png >> bundled.go
+fyne bundle -append icons/heart-outline.png >> bundled.go
+fyne bundle -append icons/heart-outline-invert.png >> bundled.go
+fyne bundle -append icons/musicnotes.png >> bundled.go
+fyne bundle -append icons/musicnotes-invert.png >> bundled.go
+fyne bundle -append icons/people.png >> bundled.go
+fyne bundle -append icons/people-invert.png >> bundled.go
+fyne bundle -append icons/playlist.png >> bundled.go
+fyne bundle -append icons/playlist-invert.png >> bundled.go
+fyne bundle -append icons/podcast.png >> bundled.go
+fyne bundle -append icons/podcast-invert.png >> bundled.go
+fyne bundle -append icons/star-filled.png >> bundled.go
+fyne bundle -append icons/star-filled-invert.png >> bundled.go
+fyne bundle -append icons/star-outline.png >> bundled.go
+fyne bundle -append icons/star-outline-invert.png >> bundled.go
+fyne bundle -append icons/theatermasks.png >> bundled.go
+fyne bundle -append icons/theatermasks-invert.png >> bundled.go
