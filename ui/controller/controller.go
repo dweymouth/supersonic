b5aa8e61f0312c2308247979c9ef207761bf6e72
Drew Weymouth <dweymouth@gmail.com>
2023-02-12 17:21:47 -0800

    refactor login workflow, can now re-prompt for password if missing

--- ui/controller/controller.go
+++ ui/controller/controller.go
@@ -38,6 +38,21 @@ func (m Controller) ShowPopUpImage(img image.Image) {
 	))
 }
 
+func (m Controller) PromptForFirstServer() {
+	d := dialogs.NewAddEditServerDialog("Connect to Server", nil)
+	pop := widget.NewModalPopUp(d, m.MainWindow.Canvas())
+	d.OnSubmit = func() {
+		pop.Hide()
+		server := m.App.Config.AddServer(d.Nickname, d.Host, d.Username)
+		if err := m.App.ServerManager.SetServerPassword(server, d.Password); err != nil {
+			log.Printf("error setting keyring credentials: %v", err)
+			// TODO: handle?
+		}
+		m.DoConnectToServerWorkflow(server)
+	}
+	pop.Show()
+}
+
 // Show dialog to prompt for playlist.
 // Depending on the results of that dialog, potentially create a new playlist
 // Add tracks to the user-specified playlist
@@ -67,4 +82,52 @@ func (m Controller) DoAddTracksToPlaylistWorkflow(trackIDs []string) {
 		}
 	}
 	pop.Show()
+}
+
+func (c Controller) DoConnectToServerWorkflow(server *backend.ServerConfig) {
+	pass, err := c.App.ServerManager.GetServerPassword(server)
+	if err != nil {
+		log.Printf("error getting password from keyring: %v", err)
+		c.promptForLogin(func(server *backend.ServerConfig, password string) {
+			if err := c.App.ServerManager.SetServerPassword(server, password); err != nil {
+				log.Printf("error setting keyring credentials: %v", err)
+				// TODO: handle?
+			}
+			c.tryConnectToServer(server, password)
+		})
+	} else {
+		c.tryConnectToServer(server, pass)
+	}
+}
+
+func (m Controller) promptForLogin(cb func(server *backend.ServerConfig, password string)) {
+	// TODO: this will need to be rewritten a bit when we support multi servers
+	// need to make sure the intended server is first in the list passed to NewLoginDialog
+	d := dialogs.NewLoginDialog(m.App.Config.Servers)
+	pop := widget.NewModalPopUp(d, m.MainWindow.Canvas())
+	d.OnSubmit = func(server *backend.ServerConfig, password string) {
+		pop.Hide()
+		cb(server, password)
+	}
+	d.OnEditServer = func(server *backend.ServerConfig) {
+		pop.Hide()
+		editD := dialogs.NewAddEditServerDialog("Edit server", server)
+		editPop := widget.NewModalPopUp(editD, m.MainWindow.Canvas())
+		editD.OnSubmit = func() {
+			editPop.Hide()
+			server.Hostname = editD.Host
+			server.Nickname = editD.Nickname
+			server.Username = editD.Username
+			cb(server, editD.Password)
+		}
+		editPop.Show()
+	}
+	pop.Show()
+}
+
+func (c Controller) tryConnectToServer(server *backend.ServerConfig, password string) {
+	if err := c.App.ServerManager.ConnectToServer(server, password); err != nil {
+		log.Printf("error connecting to server: %v", err)
+		// TODO: surface error to user
+	}
 }
