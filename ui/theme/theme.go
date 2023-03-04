4eddf262cb585faede5ebd2fb792e444966b678b
Drew Weymouth <dweymouth@gmail.com>
2023-03-04 10:34:01 -0800

    color adjustments

--- ui/theme/theme.go
+++ ui/theme/theme.go
@@ -16,13 +16,15 @@ var _ fyne.Theme = (*MyTheme)(nil)
 func (m MyTheme) Color(name fyne.ThemeColorName, variant fyne.ThemeVariant) color.Color {
 	switch name {
 	case ColorNamePageBackground:
-		return color.RGBA{R: 20, G: 20, B: 20, A: 255}
+		return color.RGBA{R: 15, G: 15, B: 15, A: 255}
 	case theme.ColorNameBackground:
-		return color.RGBA{R: 40, G: 40, B: 40, A: 255}
+		return color.RGBA{R: 30, G: 30, B: 30, A: 255}
 	case theme.ColorNameScrollBar:
 		return theme.DarkTheme().Color(theme.ColorNameForeground, variant)
 	case theme.ColorNameButton:
-		return theme.DarkTheme().Color(theme.ColorNameInputBackground, variant)
+		return color.RGBA{R: 20, G: 20, B: 20, A: 50}
+	case theme.ColorNameInputBackground:
+		return color.RGBA{R: 20, G: 20, B: 20, A: 50}
 	}
 	return theme.DarkTheme().Color(name, variant)
 }
