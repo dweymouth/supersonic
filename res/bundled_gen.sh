d3b58147de012160c25ba406d7283eabfefde4fb
Drew Weymouth <dweymouth@gmail.com>
2023-03-04 16:19:34 -0800

    add app icon to bundled resources

--- res/bundled_gen.sh
+++ res/bundled_gen.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-fyne bundle -package res -prefix Res icons/freepik/disc.png > bundled.go
+fyne bundle -package res -prefix Res appicon-250.png > bundled.go
+fyne bundle -append -prefix Res icons/freepik/disc.png >> bundled.go
 fyne bundle -append -prefix Res icons/freepik/disc-invert.png >> bundled.go
 fyne bundle -append -prefix Res icons/freepik/headphones.png >> bundled.go
 fyne bundle -append -prefix Res icons/freepik/headphones-invert.png >> bundled.go
