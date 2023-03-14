20f274797f1202529dfc367620b6b77a36198f48
Drew Weymouth <dweymouth@gmail.com>
2023-03-14 10:23:44 -0700

    move SearchEntry into searcher.go file

--- ui/widgets/searcher.go
+++ ui/widgets/searcher.go
@@ -3,6 +3,10 @@ package widgets
 import (
 	"sync"
 	"time"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
 )
 
 type Searcher struct {
@@ -59,5 +63,58 @@ func (s *Searcher) waitAndSearch() {
 func (s *Searcher) sendSearch(text string) {
 	if s.OnSearched != nil {
 		s.OnSearched(text)
+	}
+}
+
+type SearchEntry struct {
+	widget.Entry
+	height float32
+}
+
+func NewSearchEntry() *SearchEntry {
+	sf := &SearchEntry{}
+	sf.ExtendBaseWidget(sf)
+	// this is a bit hacky
+	sf.height = widget.NewEntry().MinSize().Height
+	sf.PlaceHolder = "Search"
+	c := NewClearTextButton()
+	c.OnTapped = func() {
+		sf.SetText("")
+	}
+	sf.ActionItem = c
+	return sf
+}
+
+func (s *SearchEntry) Refresh() {
+	if s.Text == "" {
+		s.ActionItem.(*clearTextButton).Resource = theme.SearchIcon()
+	} else {
+		s.ActionItem.(*clearTextButton).Resource = theme.ContentClearIcon()
+	}
+	s.Entry.Refresh()
+}
+
+func (s *SearchEntry) MinSize() fyne.Size {
+	return fyne.NewSize(200, s.height)
+}
+
+var _ fyne.Tappable = (*clearTextButton)(nil)
+
+type clearTextButton struct {
+	widget.Icon
+
+	OnTapped func()
+}
+
+func NewClearTextButton() *clearTextButton {
+	c := &clearTextButton{}
+	c.ExtendBaseWidget(c)
+	c.Resource = theme.SearchIcon()
+	return c
+}
+
+func (c *clearTextButton) Tapped(*fyne.PointEvent) {
+	if c.OnTapped != nil {
+		c.OnTapped()
 	}
 }
