15bff7c1ae77a5d97c70db37d8431a5d2628120d
Drew Weymouth <dweymouth@gmail.com>
2023-11-13 18:03:57 -0800

    always save config file on server connected to ensure we don't lose server settings with abnormal exit

--- ui/mainwindow.go
+++ ui/mainwindow.go
@@ -102,13 +102,11 @@ func NewMainWindow(fyneApp fyne.App, appName, displayAppName, appVersion string,
 				m.ShowWhatsNewDialog()
 			}
 			m.App.Config.Application.LastLaunchedVersion = app.VersionTag()
-			m.App.SaveConfigFile()
 		} else if t := app.UpdateChecker.VersionTagFound(); t != "" && t != app.Config.Application.LastCheckedVersion {
 			if t != app.VersionTag() {
 				m.ShowNewVersionDialog(displayAppName, t)
 			}
 			m.App.Config.Application.LastCheckedVersion = t
-			m.App.SaveConfigFile()
 		}
 		// register callback for the ongoing periodic update check
 		m.App.UpdateChecker.OnUpdatedVersionFound = func() {
@@ -118,6 +116,7 @@ func NewMainWindow(fyneApp fyne.App, appName, displayAppName, appVersion string,
 			}
 			m.App.Config.Application.LastCheckedVersion = t
 		}
+		m.App.SaveConfigFile()
 	})
 	app.ServerManager.OnLogout(func() {
 		m.BrowsingPane.DisableNavigationButtons()
