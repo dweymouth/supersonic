bd4db95f64bdf1aa26977f1dcfaa69667c97f404
Drew Weymouth <dweymouth@gmail.com>
2023-12-18 07:52:59 -0800

    fix non-Mac build

--- backend/mpmedia_other.go
+++ backend/mpmedia_other.go
@@ -5,10 +5,10 @@ package backend
 import (
 	"errors"
 
-	"github.com/dweymouth/supersonic/player"
+	"github.com/dweymouth/supersonic/player/mpv"
 )
 
-func InitMPMediaHandler(player *player.Player, playbackManager *PlaybackManager, artURLLookup func(trackID string) (string, error)) error {
+func InitMPMediaHandler(player *mpv.Player, playbackManager *PlaybackManager, artURLLookup func(trackID string) (string, error)) error {
 	// MPMediaHandler only supports macOS.
 	return errors.New("unsupported platform")
 }
