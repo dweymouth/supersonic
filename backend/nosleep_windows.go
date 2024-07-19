80e53c2971f512519f458ff53d3b9ab25b47f83d
Drew Weymouth <dweymouth@gmail.com>
2024-07-19 07:29:57 -0700

    load DLL only once

--- backend/nosleep_windows.go
+++ backend/nosleep_windows.go
@@ -2,16 +2,30 @@
 
 package backend
 
-import "syscall"
+import (
+	"sync/atomic"
+	"syscall"
+)
 
 const (
 	ES_CONTINUOUS      uint = 0x80000000
 	ES_SYSTEM_REQUIRED uint = 0x00000001
 )
 
+var (
+	sleepDisabled  atomic.Bool
+	executionState *syscall.LazyProc
+)
+
 func SetSystemSleepDisabled(disable bool) {
-	kernel32 := syscall.NewLazyDLL("kernel32.dll")
-	executionState := kernel32.NewProc("SetThreadExecutionState")
+	if old := sleepDisabled.Swap(disable); old == disable {
+		return
+	}
+
+	if executionState == nil {
+		kernel32 := syscall.NewLazyDLL("kernel32.dll")
+		executionState = kernel32.NewProc("SetThreadExecutionState")
+	}
 
 	uType := ES_CONTINUOUS
 	if disable {
