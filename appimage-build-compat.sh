dac77a0dd658f2c2008e5ac41b6b00ee73d1988d
Masterlocker <wsurew@gmail.com>
2024-09-14 15:11:26 +0200

    Update appimage-build-compat.sh

--- appimage-build-compat.sh
+++ appimage-build-compat.sh
@@ -79,7 +79,7 @@ cp /usr/lib/x86_64-linux-gnu/libsratom-0.so.0 Supersonic.AppDir/usr/lib/ #
 cp /usr/lib/x86_64-linux-gnu/libpgm-5.3.so.0 Supersonic.AppDir/usr/lib/ #
 cp /usr/lib/x86_64-linux-gnu/libnorm.so.1 Supersonic.AppDir/usr/lib/ #
 #cp /usr/lib/x86_64-linux-gnu/libmbedcrypto.so.7 Supersonic.AppDir/usr/lib/
-#cp /usr/lib/x86_64-linux-gnu/libcjson.so.1 Supersonic.AppDir/usr/lib/
+cp /usr/lib/x86_64-linux-gnu/librom1394.so.0 Supersonic.AppDir/usr/lib/
 cp /usr/lib/x86_64-linux-gnu/libsndio.so.7 Supersonic.AppDir/usr/lib/
 cp /usr/lib/x86_64-linux-gnu/libflite_cmu_us_kal16.so.1 Supersonic.AppDir/usr/lib/ #
 #cp /usr/lib/x86_64-linux-gnu/libhwy.so.1 Supersonic.AppDir/usr/lib/
