4c1a95e851d94744e5c73c778bc26177f0988d31
Drew Weymouth <dweymouth@users.noreply.github.com>
2025-09-24 18:59:55 -0700

    add settings for skipping one-star or tracks with keyword (#724)

--- backend/config.go
+++ backend/config.go
@@ -112,9 +112,11 @@ type NowPlayingPageConfig struct {
 }
 
 type PlaybackConfig struct {
-	Autoplay           bool
-	RepeatMode         string
-	UseWaveformSeekbar bool
+	Autoplay                 bool
+	RepeatMode               string
+	SkipOneStarWhenShuffling bool
+	SkipKeywordWhenShuffling string
+	UseWaveformSeekbar       bool
 }
 
 type LocalPlaybackConfig struct {
